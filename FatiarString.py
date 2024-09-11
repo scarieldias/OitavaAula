ef855ecaaaf10dedd8089f70fde867a09bd708b3
scarieldias <alessadro.scariel@gmail.com>
2024-09-11 20:54:51 -0300

    Imprime nome do meio , cumprimento e separacao

---
+++ FatiarString.py
@@ -0,0 +1,9 @@
+# Imprime nome do meio
+
+nome = "Alessandro Scariel Dias"
+tamanho = len(nome)
+
+print(nome[11:18])
+
+print ("O numero de elementos do nome é .: " , tamanho)
+print ("A nova saida é .: " , nome.split())
