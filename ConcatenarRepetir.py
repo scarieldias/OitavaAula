5a6583a3e383314ce99e82473b5b473db234a4c8
scarieldias <alessadro.scariel@gmail.com>
2024-09-11 20:27:11 -0300

    Metodo que mosntra na tela pelo numero de vezes desejado

---
+++ ConcatenarRepetir.py
@@ -0,0 +1,8 @@
+# Recebe String e repete pelo numero digitado
+
+palavra = input("Digite palavra desejada.: ")
+numero = int(input("Digite o numero de vezes que deseja repetir.: "))
+eco = palavra * numero
+
+
+print(f"A palavra é :{eco}")
