9ea77d9795de5ede1ec5412442f1c476d1bdc68d
scarieldias <alessadro.scariel@gmail.com>
2024-09-11 21:32:27 -0300

    Mostra append e remove de frutas

--- DesafioManipulacaLista.py
+++ DesafioManipulacaLista.py
@@ -1,4 +1,17 @@
 # Apenda e remove
 
-frutas = {maça, banana, laranja}
+frutas = ["maca", "banana", "laranja"]
 
+adicionar = input("Que fruta deseja adicionar? ")
+
+frutas.append(adicionar)
+
+print(frutas)
+
+remover = input("Qual fruta deseja remover? ")
+
+if remover in frutas:
+    frutas.remove(remover)
+    print("Item removido da lista. Lista atualizada: " , frutas)
+else:
+    print("Fruta nao existe na lista")
